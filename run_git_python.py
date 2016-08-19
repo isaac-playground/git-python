049759986a3f348da83bc4ef02cd5149921812d3
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2016-08-19 10:34:23 -0400

    Manual commit to add the initial functionality

---
+++ run_git_python.py
@@ -0,0 +1,31 @@
+"""
+"""
+import os.path
+import git
+
+REPO_ROOT = os.path.abspath(os.path.dirname(__file__))
+DATA_DIR = os.path.join(REPO_ROOT, 'data')
+CURRENT_EXECUTION_VERSION = 3
+
+
+print("Repo root: " + REPO_ROOT)
+print("Data directory: " + DATA_DIR)
+
+repo = git.Repo(REPO_ROOT)
+git_driver = repo.git
+
+# Create a new file and commit it to the repo.
+new_file = os.path.join(DATA_DIR, "created {number}.txt".format(number=CURRENT_EXECUTION_VERSION))
+old_file = os.path.join(DATA_DIR, "created {number}.txt".format(number=CURRENT_EXECUTION_VERSION-1))
+with open(new_file, mode='w') as fout:
+    contents = "Created file {number}".format(number=CURRENT_EXECUTION_VERSION)
+    fout.write(contents)
+
+if os.path.exists(old_file):
+    print("Removing file: " + old_file)
+    os.remove(old_file)
+
+
+print("Repo is dirty: " + repr(repo.is_dirty()))
+print(git_driver.add('.'))
+git_driver.commit('-a -m "Execution {index}"'.format(index=CURRENT_EXECUTION_VERSION))
